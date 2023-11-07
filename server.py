e23716dcd7a8761cc2e9ed3085bef94796c810c2
Peter Holdsworth <peter.j.holdsworth@gmail.com>
2023-11-07 09:30:10 +0000

    Create server.py

---
+++ server.py
@@ -0,0 +1,46 @@
+import fastapi
+import torch
+# import tokenizer
+
+
+app = fastapi.FastAPI()
+
+# run with - uvicorn server:app --host 0.0.0.0 --port 8080 --reload
+@app.on_event("startup")
+async def startup_event():
+  print("Starting Up")
+
+  # app.state.tokenizer = tokenizer.Tokenizer()
+  # app.state.tokenizer.embeddings.eval()
+  
+  # app.state.model.load_state_dict(torch.load("./model?????.pt"))
+  # app.state.model.eval()
+
+
+
+@app.on_event("shutdown")
+async def startup_event():
+  print("Shutting down")
+
+
+@app.get("/")
+def on_root():
+  return { "message": "Hello App" }
+
+
+@app.post("/learn_to_search")
+async def learn_to_search(request: fastapi.Request):
+
+  # query is in text field?
+  query = (await request.json())["text"]
+
+  # query_tokens = app.state.tokenizer.encode(query)
+  # query_embeddings = app.state.tokenizer.embeddings(query_tokens)
+
+  # document_indexes = app.state.model(query_embedddings)
+
+  # return extended passage
+  result = ["Mary had a little lamb"]
+  print("query", query)
+  print("result", result)
+  return result
