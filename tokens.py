8703bc8a74e5b6739a5904dc4aaaf5a3be9b5355
willparker-and <william.parker@and.digital>
2023-11-09 17:46:42 +0000

    Changing input sentence size

--- tokens.py
+++ tokens.py
@@ -1,5 +1,7 @@
 from datasets import load_dataset
 import sentencepiece as spm
+import constants
+
 dataset = load_dataset("roneneldan/TinyStories")
 
 model_prefix = "tinystorycustom"
@@ -16,5 +18,7 @@ spm.SentencePieceTrainer.train(
     input=text_file,  # Pass the text data from the DataFrame
     model_prefix=model_prefix,
     vocab_size=vocab_size,
-    pad_id=3
+    pad_id=3,
+    input_sentence_size=constants.INPUT_SENTENCE_SIZE,
+    shuffle_input_sentence=True
 )
