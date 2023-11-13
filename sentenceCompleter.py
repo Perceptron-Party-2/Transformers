29b054d5ec69912db6783fbf0a1847a6ab991a88
willparker-and <william.parker@and.digital>
2023-11-13 12:48:35 +0000

    Added code in to handle GPU into sentenceCompleter

--- sentenceCompleter.py
+++ sentenceCompleter.py
@@ -10,17 +10,20 @@ import utilities
 # Generate random sample data
 # tgt_data = torch.randint(1, tgt_vocab_size, (64, max_seq_length))  # (batch_size, seq_length)
 
+device = utilities.getDevice()
+print(f"Device = {device}")
+
 def generate(text):
 
   sp = spm.SentencePieceProcessor(model_file='tinystorycustom.model')
   encodedSentence = torch.tensor(sp.encode_as_ids(text, add_bos=True)).long().unsqueeze(0)
-  transformer = Transformer(constants.VOCAB_SIZE, constants.DIMENSIONS, constants.NUM_HEADS, constants.NUM_LAYERS, constants.D_FF, constants.MAX_SEQ_LENGTH, constants.DROPOUT)
+  transformer = Transformer(constants.VOCAB_SIZE, constants.DIMENSIONS, constants.NUM_HEADS, constants.NUM_LAYERS, constants.D_FF, constants.MAX_SEQ_LENGTH, constants.DROPOUT).to(device)
   transformer.eval()
   utilities.load_latest_checkpoint(transformer)
   
   for _ in range(20):
     with torch.no_grad():
-      logits = transformer(encodedSentence)
+      logits = transformer(encodedSentence.to(device))
       logits = logits[:, -1, :] / 1.0
       probs = torch.nn.functional.softmax(logits, dim=-1)
       next = torch.multinomial(probs, num_samples=1)
